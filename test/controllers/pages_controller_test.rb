9e42f19e5adc68a104bdb35dbc375053853fe846
andrewgy8 <andrewgy8@gmail.com>
2016-05-24 18:20:37 +0200

    Basic layout of site

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,29 +1,37 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+  def title
+    @base_title = "| AGY"
+  end
   test "home page" do
     get :home
-    assert_response :success 
+    assert_response :success
+    assert_select ("title"), "Home #{@base_title}" 
   end
 
   test "about page" do
     get :about
     assert_response :success 
+    assert_select ("title"), "About #{@base_title}"
   end
 
   test "projects page" do
     get :projects
     assert_response :success 
+    assert_select ("title"), "Projects #{@base_title}"
   end
 
   test "contact page" do
     get :contact
     assert_response :success 
-    
+    assert_select ("title"), "Contact #{@base_title}"
   end
 
   test "show page" do
     get :show
-    assert_response :success 
+    assert_response :success
+    assert_select ("title"), "Show #{@base_title}" 
   end
+  
 end

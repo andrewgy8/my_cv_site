4d1c8035edad3490dd909c2f0db90c54f8b7b767
andrewgy8 <andrewgy8@gmail.com>
2016-05-24 13:55:23 +0200

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+#Contact info for myself
+  def contact
+  end
+
+#this will be to show my CV
+  def about
+  end
+
+  def show
+  end
+
+  def projects
+  end
+  
+end

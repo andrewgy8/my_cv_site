5ac38148a00d02042a106cf0f151198b49629dd9
andrewgy8 <andrewgy8@gmail.com>
2016-05-24 14:14:05 +0200

    Basic testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root                 'pages#home'
+  get 'projects'    => 'pages#projects'
+  get 'about'       => 'pages#about'
+  get 'contact'     => 'pages#contact'
+  get 'show'        => 'pages#show'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "home page" do
+    get :home
+    assert_response :success 
+  end
+
+  test "about page" do
+    get :about
+    assert_response :success 
+  end
+
+  test "projects page" do
+    get :projects
+    assert_response :success 
+  end
+
+  test "contact page" do
+    get :contact
+    assert_response :success 
+    
+  end
+
+  test "show page" do
+    get :show
+    assert_response :success 
+  end
 end
